385ab4a46bce24bc362dd0cfda3b575d7c512a41
Irakli Bokuchava <bokuchavairakli42@gmail.com>
2022-05-10 11:51:49 +0430

    Add basic functionality to login page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,8 +1,29 @@
-import React from "react";
-import "./login.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./login.css";
 
 export default function Login() {
+	// States
+	const [cell_phone_input, set_cell_phone_input] = useState("");
+	const [password_input, set_password_input] = useState("");
+	const [cell_phone_warning, set_cell_phone_warning] = useState("");
+	const [password_warning, set_password_warning] = useState("");
+	// States functions
+	const check_form = (e) => {
+		e.preventDefault();
+		// Cellphone input
+		if (cell_phone_input !== "") {
+			set_cell_phone_warning(false);
+		} else {
+			set_cell_phone_warning(true);
+		}
+		// Password input
+		if (password_input !== "") {
+			set_password_warning(false);
+		} else {
+			set_password_warning(true);
+		}
+	};
 	return (
 		<div className="whole-login-container">
 			<div
@@ -30,7 +51,18 @@ export default function Login() {
 						<label htmlFor="email" className="label">
 							موبایل
 						</label>
-						<input type="number" className="login-page-number-input"></input>
+						<input
+							type="number"
+							className="login-page-number-input"
+							onChange={(e) => {
+								set_cell_phone_input(e.target.value);
+							}}
+						/>
+						{cell_phone_warning ? (
+							<span className="warning">شماره موبایل را بدرستی درج کنید</span>
+						) : (
+							""
+						)}
 					</div>
 					<div className="login-page-form-input">
 						<label htmlFor="password" className="label">
@@ -39,8 +71,16 @@ export default function Login() {
 						<input
 							type="password"
 							className="login-page-password-input"
-						></input>
+							onChange={(e) => {
+								set_password_input(e.target.value);
+							}}
+						/>
 					</div>
+					{password_warning ? (
+						<span className="warning">کلمه عبور را بدرستی درج کنید</span>
+					) : (
+						""
+					)}
 					<div className="display-space-between">
 						<div>
 							<input type="checkbox"></input>
@@ -55,7 +95,13 @@ export default function Login() {
 						</div>
 					</div>
 					<div>
-						<button type="submit" className="login-page-submit-button">
+						<button
+							type="submit"
+							className="login-page-submit-button"
+							onClick={(e) => {
+								check_form(e);
+							}}
+						>
 							ورود
 						</button>
 						<Link to="/sign-up">
