c0c9c7c964a23639fc01b14bf38dcbbd55c2998b
Irakli Bokuchava <bokuchavairakli42@gmail.com>
2022-05-15 11:33:04 +0430

    Build table component and style it with proper commenting

---
+++ src/Components/Table/Table.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./table.css";
+export default function Table(props) {
+	const currencies_info = props.currencies_api;
+	return (
+		<div className="table-container component_box_shadow">
+			<table>
+				{/* Table header */}
+				<tr className="table-header">
+					<th>ارز</th>
+					<th>نام ارز</th>
+					<th>قیمت خرید از ما </th>
+					<th>قیمت فروش به ما </th>
+					<th>موجودی</th>
+				</tr>
+				{/* Each currency row */}
+				{currencies_info.map((item) => {
+					return (
+						<>
+							<tr className="table-each-currency-row">
+								<td>
+									<img
+										src={item.icon_source}
+										alt=""
+										width="30px"
+										height="40px"
+									/>
+								</td>
+								<td>{item.persian_name}</td>
+								<td>{item.world_price}</td>
+								<td>{item.website_price}</td>
+								<td>{item.our_inventory}</td>
+							</tr>
+						</>
+					);
+				})}
+			</table>
+		</div>
+	);
+}
