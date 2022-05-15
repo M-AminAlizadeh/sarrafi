40b2ef13a3b3c93c27b203b7dc6ba6033c5292a6
Irakli Bokuchava <bokuchavairakli42@gmail.com>
2022-05-15 10:31:47 +0430

    Add comments to jsx and get api from App.js

--- src/Components/Converter/Converter.js
+++ src/Components/Converter/Converter.js
@@ -1,64 +1,15 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./converter.css";
 
-// Artificial object about currencies information
-const currencies_info = [
-	{
-		id: 1,
-		name: "بیت کوین (bitcoin)",
-		icon: "images/crypto-logos/bitcoin.png",
-		abbreviation: "BTC",
-		world_price: 39309.13,
-		website_price: "3000",
-		balance: 0,
-		in_tomans: 0,
-	},
-	{
-		id: 2,
-		name: "اتریوم (ethereum)",
-		icon: "images/crypto-logos/ethereum.png",
-		abbreviation: "ETH",
-		world_price: 39309.13,
-		website_price: "90",
-		balance: 0,
-		in_tomans: 0,
-	},
-	{
-		id: 3,
-		name: "تتر (tether)",
-		icon: "images/crypto-logos/tether.png",
-		abbreviation: "USDT",
-		world_price: 39309.13,
-		website_price: "5",
-		balance: 0,
-		in_tomans: 0,
-	},
-	{
-		id: 4,
-		name: "دوج کوین (dogecoin)",
-		icon: "images/crypto-logos/dogecoin.png",
-		abbreviation: "DOGE",
-		world_price: 39309.13,
-		website_price: "1000000",
-		balance: 0,
-		in_tomans: 0,
-	},
-	{
-		id: 5,
-		name: "ریپل (ripple)",
-		icon: "images/crypto-logos/xrp.png",
-		abbreviation: "XRP",
-		world_price: 39309.13,
-		website_price: "1,108",
-		balance: 0,
-		in_tomans: 0,
-	},
-];
-export default function Converter() {
+export default function Converter(props) {
+	const currencies_info = props.currencies_api;
 	// States
 	const [api_data, set_api_data] = useState(currencies_info);
 	const [toggle_drop_down, set_toggle_drop_down] = useState(false);
-	const [currency_name, set_currency_name] = useState(currencies_info[0].name);
+	const [currency_name, set_currency_name] = useState(
+		currencies_info[0].persian_name
+	);
 	const [currency_icon, set_currency_icon] = useState(currencies_info[0].icon);
 	const [currency_value, set_currency_value] = useState(
 		currencies_info[0].website_price
@@ -75,7 +26,7 @@ export default function Converter() {
 		set_toggle_drop_down(false);
 		api_data.map((item) => {
 			if (e.target.id == item.id) {
-				set_currency_name(item.name);
+				set_currency_name(item.persian_name);
 				set_currency_icon(item.icon);
 				set_currency_value(item.website_price);
 				set_toman_value(item.website_price);
@@ -87,116 +38,142 @@ export default function Converter() {
 		// console.log(e.target.value);
 		const user_input = e.target.value;
 		const filtered_list = currencies_info.filter((item) => {
-			return item.name.includes(user_input);
+			return item.persian_name.includes(user_input);
 		});
 		set_api_data(filtered_list);
 	};
 	// This function covert unit to toman
 	const unit_to_toman_converter = (e) => {
-		set_unit_value(e.target.value);
-		set_toman_value(e.target.value * currency_value);
+		// console.log(typeof e.target.value);
+		console.log(toman_value);
+		set_unit_value(Number(e.target.value));
+		set_toman_value(Number(e.target.value) * Number(currency_value));
 	};
 	// This function covert toman to unit
 	const toman_to_unit_converter = (e) => {
-		set_unit_value(e.target.value / currency_value);
-		set_toman_value(e.target.value);
+		set_unit_value(Number(e.target.value) / Number(currency_value));
+		set_toman_value(Number(e.target.value));
 	};
 	return (
-		<div className="converter-container">
+		<div className="converter-container component_box_shadow">
 			<div className="converter-inner-container">
-				<fieldset className="converter-choose-currency">
-					<legend>انتخاب ارز</legend>
-					{/* Dropdown box */}
-					<div className="converter-drop-down-box" onClick={toggle}>
-						{/* Currency icon */}
-						<div className="currency-icon-container">
-							<img src={currency_icon} alt={currency_name} width="50px" />
-						</div>
-						{/* Currency persian name */}
-						<div className="currency-persian-name-container">
-							<span className="currency-persian-name">{currency_name}</span>
-						</div>
-						{/* Currency chevron down */}
-						{toggle_drop_down ? (
-							<div className="currency-chevron-container">
+				<div class="converter-top-row">
+					{/* Converter Choose Currency */}
+					<fieldset className="converter-choose-currency">
+						<legend>انتخاب ارز:</legend>
+						{/* Dropdown box */}
+						<div className="converter-drop-down-box" onClick={toggle}>
+							{/* Currency icon */}
+							<div className="converter-currency-icon-container">
+								<img src={currency_icon} alt={currency_name} width="30px" />
+							</div>
+							{/* Currency persian name */}
+							<div className="currency-persian-name-container">
+								<span className="currency-persian-name">{currency_name}</span>
+							</div>
+							{/* Currency chevron down */}
+							<div
+								className={`converter-currency-chevron-container ${
+									toggle_drop_down ? "chevron-container-toggle" : null
+								}`}
+							>
 								<img src="https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/24/000000/external-chevron-arrows-tanah-basah-basic-outline-tanah-basah-2.png" />
 							</div>
-						) : (
-							<div className="currency-chevron-container">
-								<img src="https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/24/000000/external-chevron-arrows-tanah-basah-basic-outline-tanah-basah-4.png" />
-							</div>
-						)}
-					</div>
-					{/* Dropdown itself */}
-					{toggle_drop_down ? (
-						<div className="converter-drop-down">
-							{/* Searchbar */}
-							<input
-								type="search"
-								name=""
-								id=""
-								placeholder="جستجو بر اساس اسم یا نماد..."
-								onChange={(e) => {
-									filter_items(e);
-								}}
-							/>
-							{api_data.map((item) => {
-								return (
-									<div
-										className="drop-down-row"
-										onClick={(e) => {
-											choose_currency(e);
-										}}
-										id={item.id}
-									>
-										<img
-											id={item.id}
-											src={item.icon}
-											alt={item.name}
-											width="20px"
-										/>
-										<span id={item.id} className="currency-name">
-											{item.name}
-										</span>
-										<span id={item.id} className="currency-abbr">
-											{item.abbreviation}
-										</span>
-										<span id={item.id} className="currency-price">
-											قیمت خرید:
-											{item.website_price}تومان
-										</span>
-									</div>
-								);
-							})}
 						</div>
-					) : (
-						""
-					)}
-				</fieldset>
-				{/* Converter Unit */}
-				<fieldset className="converter-unit">
-					<legend>واحد</legend>
-					<input
-						type="number"
-						className="converter-unit-input"
-						value={unit_value}
-						onChange={(e) => {
-							unit_to_toman_converter(e);
-						}}
-					/>
-				</fieldset>
-				{/* Converter Toman */}
-				<fieldset className="converter-toman">
-					<legend>تومان</legend>
-					<input
-						type="number"
-						className="converter-toman-input"
-						value={toman_value}
-						onChange={(e) => {
-							toman_to_unit_converter(e);
-						}}
-					/>
-				</fieldset>
+						{/* Dropdown itself */}
+						{toggle_drop_down ? (
+							<div className="converter-drop-down component_box_shadow">
+								{/* Searchbar */}
+								<input
+									type="search"
+									placeholder="جستجو بر اساس اسم یا نماد..."
+									onChange={(e) => {
+										filter_items(e);
+									}}
+								/>
+								{api_data.map((item) => {
+									return (
+										// Dropdown Row
+										<div
+											className="converter-drop-down-row"
+											onClick={(e) => {
+												choose_currency(e);
+											}}
+											id={item.id}
+										>
+											{/* Dropdown right side */}
+											<div className="converter-drop-down-right-side">
+												{/* Currency icon */}
+												<img
+													id={item.id}
+													src={item.icon}
+													alt={item.persian_name}
+													width="20px"
+												/>
+												{/* Currency persian name */}
+												<span id={item.id} className="currency-name">
+													{item.persian_name}
+												</span>{" "}
+												{/* Currency abbreviation */}
+												<span id={item.id} className="currency-abbr">
+													({item.abbr})
+												</span>
+											</div>
+											{/* Dropdown left side */}
+											<div
+												id={item.id}
+												className="converter-drop-down-left-side"
+											>
+												<span>قیمت خرید:</span>
+												{/* Currency price */}
+												<span className="currency-price">
+													{item.website_price}تومان
+												</span>
+											</div>
+										</div>
+									);
+								})}
+							</div>
+						) : null}
+					</fieldset>
+					{/* Converter Unit */}
+					<fieldset className="converter-choose-currency converter-unit">
+						<legend>واحد:</legend>
+						<input
+							type="number"
+							className="converter-unit-input"
+							value={unit_value}
+							onChange={(e) => {
+								unit_to_toman_converter(e);
+							}}
+						/>
+					</fieldset>
+					{/* Converter Toman */}
+					<fieldset className="converter-choose-currency converter-toman">
+						<legend>تومان:</legend>
+						<input
+							type="number"
+							className="converter-toman-input"
+							value={toman_value}
+							onChange={(e) => {
+								toman_to_unit_converter(e);
+							}}
+						/>
+					</fieldset>
+				</div>
+				<div class="converter-bottom-row">
+					{/* Converter btns */}
+					<div>
+						<Link to="sell">
+							<button className="converter-btn converter-sell-btn">فروش</button>
+						</Link>
+					</div>
+					<div>
+						<Link to="buy">
+							<button className="converter-btn converter-buy-btn">خرید</button>
+						</Link>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
