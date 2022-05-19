00d8d5404c46d8a5597e4e556237b596db0c808c
M-AminAlizadeh <mohammadaminalizadeh1378@gamil.com>
2022-05-19 18:59:46 +0430

    build sell page

--- src/Pages/Buy-Sell/Sell/Sell.js
+++ src/Pages/Buy-Sell/Sell/Sell.js
@@ -1,79 +1,31 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import TrendingDownIcon from "@material-ui/icons/TrendingDown";
-import "./sell.css";
+import "../Buy/buy.css";
 import { Link } from "react-router-dom";
-import Button from "react-bootstrap/Button";
 
-// Artificial object about currencies information
-const currencies_info = [
-	{
-		id: 1,
-		name: "بیت کوین (bitcoin)",
-		icon: "images/crypto-logos/bitcoin.png",
-		abbreviation: "BTC",
-		world_price: 39309.13,
-		website_price: "3000",
-		balance: 0,
-		in_tomans: 0,
-	},
-	{
-		id: 2,
-		name: "اتریوم (ethereum)",
-		icon: "images/crypto-logos/ethereum.png",
-		abbreviation: "ETH",
-		world_price: 39309.13,
-		website_price: "90",
-		balance: 0,
-		in_tomans: 0,
-	},
-	{
-		id: 3,
-		name: "تتر (tether)",
-		icon: "images/crypto-logos/tether.png",
-		abbreviation: "USDT",
-		world_price: 39309.13,
-		website_price: "5",
-		balance: 0,
-		in_tomans: 0,
-	},
-	{
-		id: 4,
-		name: "دوج کوین (dogecoin)",
-		icon: "images/crypto-logos/dogecoin.png",
-		abbreviation: "DOGE",
-		world_price: 39309.13,
-		website_price: "1000000",
-		balance: 0,
-		in_tomans: 0,
-	},
-	{
-		id: 5,
-		name: "ریپل (ripple)",
-		icon: "images/crypto-logos/xrp.png",
-		abbreviation: "XRP",
-		world_price: 39309.13,
-		website_price: "1,108",
-		balance: 0,
-		in_tomans: 0,
-	},
-];
+export default function Sell(props) {
+	const [success_message, set_success_message] = useState(false);
 
-export default function Sell() {
+	const currencies_info = props.data;
 	/////////////////////// States
 	const [dropdown, set_drop_down] = useState(false);
 	const [api_data, set_api_data] = useState(currencies_info);
-	const [currency_icon, set_currency_icon] = useState(currencies_info[0].icon);
-	const [currency_name, set_currency_name] = useState(currencies_info[0].name);
+	const [currency_icon, set_currency_icon] = useState(
+		currencies_info[0].icon_source
+	);
+	const [currency_name, set_currency_name] = useState(
+		currencies_info[0].persian_name
+	);
 	const [currency_abbr, set_currency_abbr] = useState(currencies_info[0].abbr);
 	const [currency_price_toman, set_currency_price_toman] = useState(
 		currencies_info[0].website_price
 	);
 	const [selected, set_selected] = useState("");
-	const [currency_value, set_currency_value] = useState("");
+	const [currency_value, set_currency_value] = useState();
 	const [toman_value, set_toman_value] = useState("");
 	const [warning, set_warning] = useState(true);
 	/////////////////////// States functions
-	// this function just toggle dropdown list
+	// this function just toggles dropdown list
 	const toggle_dropdown = () => {
 		dropdown ? set_drop_down(false) : set_drop_down(true);
 	};
@@ -82,10 +34,10 @@ export default function Sell() {
 		set_drop_down(false);
 		currencies_info.map((currency) => {
 			if (e.target.id == currency.id) {
-				set_currency_name(currency.name);
-				set_currency_icon(currency.icon);
+				set_currency_name(currency.persian_name);
+				set_currency_icon(currency.icon_source);
 				set_currency_price_toman(currency.website_price);
-				set_currency_abbr(currency.abbreviation);
+				set_currency_abbr(currency.abbr);
 			}
 		});
 	};
@@ -113,24 +65,20 @@ export default function Sell() {
 		} else {
 			set_warning(true);
 		}
+		if (toman_value !== "") {
+			set_success_message(true);
+		}
 	};
 	return (
-		<div className="buy-page-container" style={{ display: "flex" }}>
-			<div
-				className="buy-page-article"
-				style={{
-					flex: 1,
-					boxShadow: " 0 0 20px #555",
-					border: "3px solid #fff",
-					backgroundColor: "#fff",
-					borderRadius: "20px",
-					padding: "20px",
-					margin: "10px",
-				}}
-			>
-				<h4 dir="rtl"> راهنمای فروش {currency_name}</h4>
+		// Page Container
+		<div className="buy-page-container">
+			{/* Left Side Container */}
+			<div className="buy-page-article">
+				{/* Article Header */}
+				<p> راهنمای فروش {currency_name}</p>
 				<hr />
-				<ul className="buy-page-list" dir="rtl">
+				{/* Article List */}
+				<ul className="buy-page-list">
 					<li>
 						کارمزد پرداختی از سوی شما برای تراکنش های رمز ارز ها کارمزد شبکه ارز
 						مدنظر میباشد و مجموعه ما از این کارمزد نفعی ندارد
@@ -156,7 +104,7 @@ export default function Sell() {
 					<br />
 					<li>
 						به این دلیل که ارز های رمزنگاری شده تحت تاثیر برخی از عوامل ممکن است
-						صعود یا سقوط ارزش را به صورت ناگهانی تجربه کنند، مجموعه ما همانگونه
+						صعود یا سقوط ارزش را به صورت ناگهانی تجربه کنند،مجموعه ما همانگونه
 						که منفعتی در قبال افزایش ارزش آن نمی برد، مسئولیتی هم در قبال سقوط
 						قیمت آن نخواهد داشت.
 					</li>
@@ -188,52 +136,38 @@ export default function Sell() {
 						اسلامی ایران است خدماتی ارائه نمیدهد
 					</li>
 				</ul>
+				<hr />
 			</div>
-			<div
-				className="buy-page-input-container"
-				dir="rtl"
-				style={{
-					flex: "2",
-					boxShadow: " 0 0 20px #555",
-					border: "3px solid #fff",
-					backgroundColor: "#fff",
-					borderRadius: "20px",
-					padding: "10px",
-					margin: "10px",
-				}}
-			>
-				<h4>
+			{/* Right side Container */}
+			<div className="buy-page-input-container component_box_shadow">
+				{/* Right side header */}
+				<p className="buy-page-input-container-header">
 					<TrendingDownIcon></TrendingDownIcon> فروش ارز
-				</h4>
-				<div
-					style={{
-						width: "100%",
-						textAlign: "center",
-						padding: "20px",
-						backgroundColor: "#EAE8FD",
-						color: "#887DF2",
-						fontSize: "17px",
-						borderRadius: "5px",
-					}}
-				>
-					مبلغ پرداختی:<span>{toman_value}</span> تومان
+				</p>
+				<div className="buy-page-payment-amount">
+					مبلغ دریافتی:<span>{toman_value}</span> تومان
 				</div>
 				{/* Select currency part */}
+				<p className="buy-page-currency-selection-header">انتخاب ارز</p>
 				<div className="currency-selection-container">
-					<span>انتخاب ارز</span>
 					{/* Main div */}
-					<div className="buy-page-input" onClick={toggle_dropdown}>
+					<div
+						className={`buy-page-select-box ${
+							dropdown ? "change-drop-down-color" : ""
+						}`}
+						onClick={toggle_dropdown}
+					>
 						{/* currency logo */}
-						<div className="currency-logo">
+						<div className="buy-page-currency-logo">
 							<img src={currency_icon} width="30px" />
 						</div>
 						{/* currency name in persian */}
-						<span className="currency-name">{currency_name}</span>
+						<span className="buy-page-currency-name">{currency_name}</span>
 						{/* currency dropdown icon */}
-						<div className="currency-dropdown">
+						<div className="buy-page-currency-dropdown">
 							<img
 								className={dropdown ? "toggle-drop-down-icon" : ""}
-								src="https://img.icons8.com/ios-glyphs/30/000000/chevron-up.png"
+								src="https://img.icons8.com/ios-glyphs/10/000000/chevron-up.png"
 							/>
 						</div>
 					</div>
@@ -243,6 +177,7 @@ export default function Sell() {
 							{/* Search box */}
 							<div className="search-box-container">
 								<input
+									style={{ cursor: "text" }}
 									type="search"
 									name="search-bar"
 									id="search-bar"
@@ -262,8 +197,14 @@ export default function Sell() {
 										id={currency.id}
 									>
 										<div class="right-side" id={currency.id}>
-											<img src={currency.icon} width="20px" id={currency.id} />
-											<span id={currency.id}>{currency.name}</span>
+											<img
+												src={currency.icon_source}
+												width="20px"
+												id={currency.id}
+											/>
+											<span id={currency.id}>
+												{currency.persian_name}({currency.name})
+											</span>
 										</div>
 										<div className="left-side" id={currency.id}>
 											<span id={currency.id}>قیمت خرید</span>
@@ -279,15 +220,8 @@ export default function Sell() {
 						""
 					)}
 				</div>
-				<div
-					style={{
-						// border: "1px solid red",
-						display: "flex",
-						width: "50%",
-						justifyContent: "space-around",
-						marginRight: "10px",
-					}}
-				>
+				{/* Price of Each Coin */}
+				<div className="buy-page-each-coin-price">
 					<p>
 						قیمت هر واحد<span className="currency-name">{currency_name}:</span>
 					</p>
@@ -300,86 +234,72 @@ export default function Sell() {
 				</div>
 				<hr />
 				<div>
-					<span>مقدار ارز</span>
-					<div
-						className={`currency-value-container ${warning ? "" : "warning"}`}
-					>
-						<label htmlFor="">مقدار {currency_name}</label>
-						<img src={currency_icon} alt={currency_name} width="20px" />
-						<input
-							type="number"
-							className="buy-page-input currency-input-output-value"
-							onChange={handle_conversion}
-							onFocus={(e) => {
-								set_selected("currency");
-							}}
-							value={currency_value}
-						/>
-						<span className="currency-abbr">{currency_abbr}</span>
+					<span className="buy-page-coin-count-header">مقدار ارز</span>
+					<div className="buy-page-coin-count-div">
+						<div className="buy-page-coin-count-container">
+							<label className="buy-page-coin-count-small-header" htmlFor="">
+								مقدار {currency_name}
+								<img src={currency_icon} alt={currency_name} width="15px" />
+							</label>
+							<input
+								placeholder={currency_abbr}
+								type="number"
+								className={`buy-page-currency-count-input  ${
+									warning ? "" : "error-input-border"
+								}`}
+								onChange={handle_conversion}
+								onFocus={(e) => {
+									set_selected("currency");
+								}}
+								value={currency_value}
+							/>
+							<span className="sell-page-span-below-coin-count-input">
+								موجودی قابل تبدیل کیف پول {currency_name} :
+							</span>
+						</div>
+						{warning ? (
+							""
+						) : (
+							<span className="buy-page-error-message error-message">
+								مقدار را وارد کنید
+							</span>
+						)}
 					</div>
-					<br></br>
-					<span>موجودی قابل تبدیل کیف پول {currency_name}:</span>
 				</div>
-				<div
-					style={{
-						width: "100%",
-						textAlign: "center",
-						padding: "10px",
-						backgroundColor: "#EAE8FD",
-						color: "#887DF2",
-						fontSize: "15px",
-						borderRadius: "5px",
-					}}
-				>
-					نکته: در رمز ارزها مقدار خریداری شده به کیف پول رمز ارز شما اضافه
-					میشود.
+				<p className="buy-page-further-description-header">توضیحات اضافه</p>
+				<div className="buy-page-text-area-container">
+					<textarea className="buy-page-text-area" rows="5"></textarea>
 				</div>
 				<div>
-					<p style={{ margin: "10px" }}>توضیحات اضافه</p>
-					<textarea
-						rows="8"
-						cols="66"
-						style={{
-							marginRight: "70px",
-							border: "1px solid #D9D9D9",
-							padding: "10px",
-							color: "#5f5f5f",
-							fontSize: "14px",
-							overflow: "auto",
-							borderRadius: "5px",
-						}}
-					></textarea>
-				</div>
-				<div>
-					<div style={{ display: "flex" }}>
-						<p>نحوه پرداخت : </p>
-						<p>
-							کیف پول (موجودی : <span>0</span>)
-						</p>
+					<div className="buy-page-payment-method-container">
+						<span>نحوه دریافت : </span>
+						<span>واریز به کیف پول</span>
 					</div>
-					<p
-						style={{
-							backgroundColor: "#F1F2F4",
-							color: "#BABFC7",
-							padding: "10px",
-							textAlign: "center",
-							borderRadius: "5px",
-						}}
-					>
+					<p className="buy-page-second-notice">
 						خرید و فروش فقط از طریق کیف پول تومانی شما انجام میشود. برای افزایش
 						موجودی به بخش
-						<Link
-							to="/wallets"
-							style={{ textDecoration: "none", color: "#5E50EE" }}
-						>
-							کیف پول تومانی
+						<Link className="buy-page-link" to="/wallets">
+							{" "}
+							کیف پول تومانی{" "}
 						</Link>
 						رجوع کنید.
 					</p>
 				</div>
-				<Button size="lg" onClick={check_form}>
+				{success_message ? (
+					<div className="buy-page-success-message success-message-container">
+						پرداخت شما با موفقیت انجام شد
+					</div>
+				) : (
+					""
+				)}
+				<button
+					className="buy-page-submit-btn"
+					type="submit"
+					size="lg"
+					onClick={check_form}
+				>
 					ثبت و پرداخت
-				</Button>
+				</button>
 			</div>
 		</div>
 	);
