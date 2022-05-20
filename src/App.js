9a675eb7767ab8d8c5f3044d53369eba271cf37a
Irakli Bokuchava <bokuchavairakli42@gmail.com>
2022-05-20 14:09:33 +0430

    Add props to Wallets component in App.js

--- src/App.js
+++ src/App.js
@@ -788,7 +788,7 @@ const AppLayout = () => (
 			/>
 			<Route
 				path="wallets"
-				element={<Wallets data={digital_currencies_arr} />}
+				element={<Wallets data={digital_currencies_arr} user_info={user_api} />}
 			/>
 			<Route path="wallets/deposit/:name" element={<Deposit />} />
 			<Route path="wallets/withdrawal/:name" element={<Withdrawal />} />
@@ -823,16 +823,21 @@ function App() {
 					<Route path="/" element={<Dashboard />} />
 					<Route
 						path="wallets"
-						element={<Wallets data={digital_currencies_arr} />}
+						element={
+							<Wallets data={digital_currencies_arr} user_info={user_api} />
+						}
 					/>
 					<Route path="wallets/deposit/:name" element={<Deposit />} />
 					<Route path="wallets/withdrawal/:name" element={<Withdrawal />} />
+					<Route path="wallets/deposit/toman" element={<Toman_deposit />} />
+					<Route
+						path="wallets/deposit/withdrawal"
+						element={<Toman_withdrawal />}
+					/>
 					<Route path="crypto-transactions" element={<Crypto_transactions />} />
 					<Route path="rial-transactions" element={<Rial_transactions />} />
 					<Route path="tickets" element={<Tickets />} />
 					<Route path="buy" element={<Buy data={digital_currencies_arr} />} />
-					<Route path="toman-deposit" element={<Toman_deposit />} />
-					<Route path="toman-withdrawal" element={<Toman_withdrawal />} />
 					<Route path="sell" element={<Sell />} />
 					<Route path="orders" element={<Orders />} />
 					<Route path="credit-cards" element={<Credit_cards />} />
