47b1acbf13aa0225710ad2ba23a9d64440f700f1
Irakli Bokuchava <bokuchavairakli42@gmail.com>
2022-05-21 15:36:45 +0430

    build components inside ttoman deposit

--- src/Pages/Main/Wallets/Toman-deposit/Toman_deposit.js
+++ src/Pages/Main/Wallets/Toman-deposit/Toman_deposit.js
@@ -1,30 +1,75 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./toman-deposit.css";
 import TrendingUpIcon from "@material-ui/icons/TrendingUp";
-export default function Toman_deposit() {
+
+export default function Toman_deposit(props) {
+	const user_info = props.user_info;
+	// States
+	const [toman_input, set_toman_input] = useState("");
+	const [error, set_error] = useState(true);
+	//States functions
+	const toman_value = (e) => {
+		const user_input = e.target.value;
+		set_toman_input(user_input);
+		console.log(error);
+	};
+	const form_checker = (e) => {
+		e.preventDefault();
+		if (toman_input == "") {
+			set_error(false);
+		} else {
+			set_error(true);
+		}
+	};
 	return (
 		<div className="toman-deposit-container" dir="rtl">
+			{/* Toman Bank portal Container */}
 			<div className="toman-deposit-right-side">
-				<h4>
+				<p className="deposit-header">
 					{" "}
-					<TrendingUpIcon /> افزایش موجودی
-				</h4>
-				<div>
-					موجودی: <span>0</span> تومان
+					<TrendingUpIcon />
+					افزایش موجودی
+				</p>
+				{/* Balance Div */}
+				<div className="deposit-form-crypto-balance">
+					موجودی: <span>{user_info.balance.rial_balance}</span> تومان
 				</div>
-				<span>واریز از درگاه</span>
-				<div>
-					<span>مبلغ</span>
-					<input type="number" />
-				</div>
-				<div>
-					<span>توضیحات اضافه</span>
-					<textarea></textarea>
-				</div>
-				<input type="submit" value="ثبت و پرداخت" />
+				{/* Form */}
+				<form className="toman-deposit-form">
+					<div className="toman-portal-header">
+						<span>واریز از درگاه</span>
+					</div>
+					<div className="toman-deposit-value">
+						<span>مبلغ</span>
+						<input
+							type="number"
+							placeholder="مبلغ به تومان"
+							className={error ? "" : "error-input-border"}
+							onChange={(e) => {
+								toman_value(e);
+							}}
+						/>
+					</div>
+					<div className="toman-deposit-explain">
+						<span>توضیحات اضافه</span>
+						<textarea cols="60" rows="10"></textarea>
+					</div>
+					<div className="deposit-submit-btn-container">
+						<button
+							type="submit"
+							onClick={(e) => {
+								form_checker(e);
+							}}
+						>
+							ثبت و پرداخت
+						</button>
+					</div>
+				</form>
 			</div>
+			{/* Toman notice container */}
 			<div className="toman-deposit-left-side">
-				<h4> نکات و هشدارها</h4>
+				<p> نکات و هشدارها</p>
 				<hr />
 				<ul>
 					<li>
