081f2a4733e9bc82cde02aa02e8333437c80bcf2
M-AminAlizadeh <mohammadaminalizadeh1378@gamil.com>
2022-05-23 15:49:07 +0430

    change password styles and move styles to css file

--- src/Pages/User-profile/Password/Password.js
+++ src/Pages/User-profile/Password/Password.js
@@ -2,105 +2,57 @@ import "./password.css";
 import { Link } from "react-router-dom";
 export default function Password() {
 	return (
-		<div
-			className="password-page-container"
-			dir="rtl"
-			style={{ display: "flex", flexWrap: "wrap" }}
-		>
-			<div
-				className="password-page-profile-card"
-				style={{
-					flex: "1",
-					border: "1px solid red",
-					direction: "rtl",
-					margin: "10px",
-					border: "3px solid #fff",
-					boxShadow: " 0 0 20px #555",
-					borderRadius: "20px",
-					color: "#626262",
-					height: "400px",
-				}}
-			>
-				<div
-					style={{
-						display: "flex",
-						flexDirection: "column",
-						justifyContent: "center",
-						alignItems: "center",
-					}}
-				>
+		// Page Container
+		<div className="password-page-container">
+			{/* Profile Card */}
+			<div className="password-page-profile-card ">
+				<div className="password-page-profile-card-inside-div">
 					<img
+						className="password-page-user-icon"
 						src="images/user-icon/user.png"
-						style={{ width: "100px", padding: "5px" }}
 					/>
-					<span className="" style={{ padding: "5px" }}>
-						امین علیزاده
-					</span>
-					<span className="" style={{ padding: "5px" }}>
-						09050287419
-					</span>
+					<span className="password-page-user-name">امین علیزاده</span>
+					<span className="password-page-user-phone-number">09050287419</span>
 				</div>
+				{/* List Container */}
 				<div className="password-page-menu-container">
-					<ul
-						style={{
-							listStyle: "none",
-							padding: "20px",
-							fontSize: "18px",
-						}}
-					>
+					<ul>
 						<Link
 							to="/authentication"
-							style={{ textDecoration: "none", color: "#626262" }}
+							className="password-page-profile-menu-link"
 						>
 							<li className="password-page-list-item">اطلاعات و احراز هویت</li>
 						</Link>
 						<Link
+							className="password-page-profile-menu-link"
 							to="/credit-cards"
-							style={{ textDecoration: "none", color: "#626262" }}
 						>
 							<li className="password-page-list-item">کارت های بانکی</li>
 						</Link>
-						<li className="password-page-list-item active">کلمه عبور</li>
+						<li className="password-page-list-item profile-menu-active">
+							کلمه عبور
+						</li>
 						<Link
+							className="password-page-profile-menu-link"
 							to="/two-factor-authentication"
-							style={{ textDecoration: "none", color: "#626262" }}
 						>
 							<li className="password-page-list-item">ورود دو مرحله‌ای</li>
 						</Link>
 					</ul>
 				</div>
 			</div>
-			<div
-				className="password-page-left-side"
-				style={{
-					flex: "3",
-					border: "1px solid red",
-					direction: "rtl",
-					margin: "10px",
-					border: "3px solid #fff",
-					boxShadow: " 0 0 20px #555",
-					borderRadius: "20px",
-					padding: "20px",
-					fontSize: "16px",
-					color: "#626262",
-					lineHeight: "1.8em",
-				}}
-			>
+			{/* Left Side */}
+			<div className="password-page-left-side component_box_shadow">
 				<p>
 					در صورت لزوم می‌توانید از طریق این صفحه کلمه عبور خود را تغییر دهید.
 					<br />
 					سعی کنید کلمه عبور پیچیده ای انتخاب کنید و با سایر حساب های شما در
 					سایت های دیگر یکسان نباشد.
-					<br /> به منظور افزایش سطح ایمنی حساب کاربری خود می توانید
-					<span style={{ color: "blue", cursor: "pointer" }}>
-						ورود دو مرحله ای
-					</span>{" "}
-					را نیز فعال کنید.
+					<br /> به منظور افزایش سطح ایمنی حساب کاربری خود می توانید ورود دو
+					مرحله ای را نیز فعال کنید.
 				</p>
-				<div
-					className="password-page-input-container"
-					style={{ textAlign: "center", margin: "100px" }}
-				>
+				{/* Input Container */}
+				<div className="password-page-input-container">
 					<div>
 						<label htmlFor="first-password-input">رمز عبور فعلی</label>
 						<input
@@ -128,11 +80,11 @@ export default function Password() {
 							className="password-page-password-input"
 						></input>
 					</div>
+					{/* Submit Button */}
 					<input
 						className="password-page-submit-button"
 						type="submit"
 						value="ثبت تغییرات"
-						style={{ borderRadius: "15px", outline: "none", margin: "20px" }}
 					></input>
 				</div>
 			</div>
