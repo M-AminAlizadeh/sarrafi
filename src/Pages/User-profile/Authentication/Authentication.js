e163663a58c0047c77387e8ba12a5187bc3e2561
M-AminAlizadeh <mohammadaminalizadeh1378@gamil.com>
2022-05-24 17:57:19 +0430

    add styles to authentication page

--- src/Pages/User-profile/Authentication/Authentication.js
+++ src/Pages/User-profile/Authentication/Authentication.js
@@ -1,6 +1,10 @@
 import "./authentication.css";
 import { useState } from "react";
+import { Calendar, DatePicker } from "react-persian-datepicker";
 export default function Authentication() {
+	const style = {
+		border: "1px solid red",
+	};
 	const [level_1, set_level_1] = useState(true);
 	const [level_2, set_level_2] = useState(false);
 
@@ -18,86 +22,44 @@ export default function Authentication() {
 	};
 
 	return (
-		<div
-			className="authentication-container"
-			dir="rtl"
-			style={{
-				width: "90%",
-				margin: "auto",
-				border: "3px solid #fff",
-				borderRadius: "20px",
-				boxShadow: " 0 0 20px #555",
-				padding: "20px",
-			}}
-		>
-			<h4>سطوح کاربری و احراز هویت</h4>
+		// Page Container
+		<div className="authentication-container component_box_shadow">
+			<DatePicker styles={style} />
+			{/* Header */}
+			<p className="authentication-header">سطوح کاربری و احراز هویت</p>
 			<p>
 				در این قسمت میتوانید احراز هویت خود را تکمیل نمایید و برای ارتقاء سطح
 				کاربری خود فقط باید اطلاعات آن سطح را تکمیل نمایید.
 			</p>
 
-			<div
-				style={{
-					border: "1px solid red",
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
-					alignItems: "center",
-				}}
-			>
-				<h1>{level_1 ? "۱" : "۲"}</h1>
+			<div className="authentication-level-number-container">
+				<p className="authentication-big-number">{level_1 ? "۱" : "۲"}</p>
 				<p>
 					حساب کاربری شما در سطح
-					<span> {level_1 ? "یک" : "دو"}</span> قراردارد.
+					<span> {level_1 ? `"یک"` : `"دو"`}</span> قراردارد.
 				</p>
-				<hr style={{ width: "50%" }} />
+				<hr />
 			</div>
-			<p>امکانات و محدودیت های سطح یک:</p>
+			<p className="authentication-list-header">
+				امکانات و محدودیت های سطح یک:
+			</p>
 			<ul>
 				<li>خرید و فروش و ترید بی نهایت رمز ارزها</li>
 				<li>ممنوع بودن برداشت رمز ارزها</li>
 				<li>ممنوع بودن خرید ارز دیجیتال</li>
 			</ul>
-			<hr />
-			<h4>سطوح کاربری و احراز هویت</h4>
+			<hr className="authentication-line-after-ul" />
+			<p className="authentication-header">سطوح کاربری و احراز هویت</p>
 			<p>
 				در هر سطح توضیحات و اطلاعات خواسته شده قابل مشاهده و تکمیل کردن است.
 			</p>
-			<div
-				className="authentication-levels-container"
-				style={{
-					display: "flex",
-					justifyContent: "space-evenly",
-					position: "relative",
-				}}
-			>
+			<div className="authentication-levels-container">
 				<div
-					className={`${level_1 ? "one-sixth" : "two-sixth"}`}
-					style={{
-						border: "1px solid #7367F0",
-						width: "90%",
-						position: "absolute",
-						top: "50%",
-						right: "0",
-						height: "3px",
-						backgroundColor: "#7367F0",
-						borderRadius: "10px",
-						zIndex: "2",
-						transition: "0.4s",
-					}}
+					className={`authentication-progress-line ${
+						level_1 ? "half" : "full"
+					}`}
 				></div>
-				<div
-					style={{
-						border: "1px solid #d9d9d9",
-						width: "100%",
-						position: "absolute",
-						top: "50%",
-						right: "0",
-						height: "3px",
-						backgroundColor: "#d9d9d9",
-						borderRadius: "10px",
-					}}
-				></div>
+				<div className="authentication-gray-bar"></div>
 				<div
 					className={`authentication-levels btn-level-one ${
 						level_1 || level_2 ? "active-border" : ""
@@ -107,7 +69,6 @@ export default function Authentication() {
 						display_1();
 					}}
 				>
-					<p>۱</p>
 					<span>سطح یک</span>
 				</div>
 				<div
@@ -119,37 +80,60 @@ export default function Authentication() {
 						display_2();
 					}}
 				>
-					<p>۲</p>
 					<span>سطح دو</span>
 				</div>
 			</div>
-
+			<hr />
 			{level_1 ? (
 				<div className="level-one-cotainer levels-info-container">
 					<p>
 						برای سطح یک فقط میبایست مشخصات زیر را تکمیل نمایید و همچنین برای
 						خرید از درگاه باید در قسمت کارت های بانکی، یک کارت بانکی ثبت کنید.
 					</p>
-					<p>امکانات و محدودیت های سطح یک:</p>
+					<p className="authentication-list-header">
+						امکانات و محدودیت های سطح یک:
+					</p>
 					<ul>
 						<li>خرید و فروش و ترید بی نهایت رمز ارزها</li>
 						<li>ممنوع بودن برداشت رمز ارزها</li>
 						<li>ممنوع بودن خرید ارز دیجیتال</li>
 					</ul>
 					<div className="authentication-level-one-input-container">
-						<label htmlFor="name">نام</label>
-						<input type="text"></input>
-						<label htmlFor="last-name">نام خانوادگی</label>
-						<input type="text"></input>
-						<label htmlFor="ID">کد ملی</label>
-						<input type="number"></input>
-						<label htmlFor="name">نام پدر</label>
-						<input type="text"></input>
-						<label htmlFor="name">
-							تاریخ تولد (هجده سال به بالا امکان ثبت وجود دارد)
-						</label>
-						<input type="text"></input>
-						<input type="submit" value="ثبت اطلاعات"></input>
+						<div
+							className="authentication-level-one-first-row"
+							style={{ flexDirection: "row" }}
+						>
+							<div>
+								<label htmlFor="name">نام</label>
+								<input type="text"></input>
+							</div>
+							<div>
+								<label htmlFor="last-name">نام خانوادگی</label>
+								<input type="text"></input>
+							</div>
+						</div>
+						<div
+							className="authentication-level-one-second-row"
+							style={{ flexDirection: "row" }}
+						>
+							<div>
+								<label htmlFor="ID">کد ملی</label>
+								<input type="number"></input>
+							</div>
+							<div>
+								<label htmlFor="name">نام پدر</label>
+								<input type="text"></input>
+							</div>
+						</div>
+						<div>
+							<label htmlFor="name">
+								تاریخ تولد (هجده سال به بالا امکان ثبت وجود دارد)
+							</label>
+						</div>
+
+						<div>
+							<input type="submit" value="ثبت اطلاعات"></input>
+						</div>
 					</div>
 				</div>
 			) : (
